2ea1459d62538faf818707caac552c356d06e44b
Sunny-zz <b695340402@163.com>
2018-06-05 11:12:24 +0800

    split cart.js

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,24 +1,53 @@
-const initialState = {
-  addId: [],
-  quantityById: {}
-}
+// const initialState = {
+//   addId: [],
+//   quantityById: {}
+// }
 
-const cart = (state = initialState, action) => {
+// const cart = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'ADD_TO_CART':
+//       const newState = { ...state }
+//       const { id } = action
+//       if (newState.addId.indexOf(id) === -1) {
+//         newState.addId.push(id)
+//       }
+//       newState.quantityById = {
+//         ...newState.quantityById,
+//         [id]: newState.quantityById[id] + 1 || 1
+//       }
+//       return newState
+//     default:
+//       return state
+//   }
+// }
+import { combineReducers } from 'redux'
+const addId = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
-      const newState = { ...state }
       const { id } = action
-      if (newState.addId.indexOf(id) === -1) {
-        newState.addId.push(id)
-      }
-      newState.quantityById = {
-        ...newState.quantityById,
-        [id]: newState.quantityById[id] + 1 || 1
+      const newState = [...state]
+      if (newState.indexOf(id) === -1) {
+        newState.push(id)
       }
       return newState
     default:
       return state
   }
 }
+const quantityById = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      const { id } = action
+      return {
+        ...state,
+        [id]: { ...state }[id] + 1 || 1
+      }
+    default:
+      return state
+  }
+}
 
-export default cart
+export default combineReducers({
+  addId,
+  quantityById
+})
